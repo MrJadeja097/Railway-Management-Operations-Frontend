2e53828205d6bb388660bb858375f25835c6ae8d
Yashrajsinh Jadeja <yashrajsinh.jadeja@esparkbizmail.com>
2025-07-16 12:16:10 +0530

    Added trains list feature, and also changes staff&active route code for making cards of them.

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,4 @@
 export * from './handler'
 export * from './ActiveRoutesApi'
 export * from './StaffApi'
+export * from './TrainApi'
--- src/features/trains/routes.tsx
+++ src/features/trains/routes.tsx
@@ -1,12 +1,12 @@
 import type { FC } from "react";
 import { Route, Routes } from "react-router";
-import { Train } from "./components";
+import { TrainComponent } from "./components";
 
 export const TrainRoutes: FC = () => {
   return (
     <>
       <Routes>
-        <Route index element={<Train />} />
+        <Route index element={<TrainComponent />} />
       </Routes>
     </>
   );
