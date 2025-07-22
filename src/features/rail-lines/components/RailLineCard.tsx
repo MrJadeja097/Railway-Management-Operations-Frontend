6f149d7b7baa8f0257790e8396b003ea6f507044
Yashrajsinh Jadeja <yashrajsinh.jadeja@esparkbizmail.com>
2025-07-22 12:15:17 +0530

    Added functionality to delete Rail line.

--- src/features/rail-lines/components/RailLineCard.tsx
+++ src/features/rail-lines/components/RailLineCard.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import type { RailLine } from "../models";
+import { useAuth } from "../../auth/AuthProvider";
+import { deleteRailLine } from "../../../api";
+import { useConfirmDelete } from "../../../Hooks";
+import { DeleteButton } from "../../../components";
 
 interface Props {
   railLine: RailLine;
+  onDeleted: () => void;
 }
 
-export const RailLineCard: React.FC<Props> = ({ railLine }) => {
+export const RailLineCard: React.FC<Props> = ({ railLine, onDeleted }) => {
+  const { token } = useAuth();
+
+  const confirmDelete = useConfirmDelete(deleteRailLine,"Rail Line",onDeleted);
+
   return (
     <div className="group bg-slate-800/60 backdrop-blur-sm rounded-2xl shadow-lg hover:shadow-xl hover:shadow-yellow-400/10 transition-all duration-300 overflow-hidden border border-slate-700/50 hover:border-yellow-400/30">
-        <div className="absolute top-4 right-4 w-16 h-14 flex items-center justify-center bg-gradient-to-br from-slate-700/80 to-slate-600/80 backdrop-blur-sm rounded-xl border border-slate-600/50 text-slate-200 text-base font-semibold group-hover:from-yellow-400/20 group-hover:to-yellow-500/20 group-hover:border-yellow-300/30 transition-all duration-300">
-          <span className="ml-1 text-lg">ID: {railLine.id}</span>
+      <div className="absolute top-4 right-4 w-16 h-14 flex items-center justify-center bg-gradient-to-br from-slate-700/80 to-slate-600/80 backdrop-blur-sm rounded-xl border border-slate-600/50 text-slate-200 text-base font-semibold group-hover:from-yellow-400/20 group-hover:to-yellow-500/20 group-hover:border-yellow-300/30 transition-all duration-300">
+        <span className="ml-1 text-lg">ID: {railLine.id}</span>
       </div>
 
       <div className="p-6">
@@ -35,6 +44,25 @@ export const RailLineCard: React.FC<Props> = ({ railLine }) => {
             {railLine.endStation.name}
           </div>
         </div>
+        <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between border-t border-slate-700/50 pt-4 text-xs text-slate-500">
+                    <span>
+                      Created{" "}
+                      {new Date(railLine.createdAt).toLocaleDateString("en-US", {
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                        hour: "numeric",
+                        minute: "numeric",
+                      })}
+                    </span>
+        
+                    {token && (
+                      <div className="mt-3 sm:mt-0">
+                        <DeleteButton onClick={() => confirmDelete(railLine.id)} />
+                        {/* <UpdateButton onClick={() => setIsEditing(true)} /> */}
+                      </div>
+                    )}
+                  </div>
       </div>
     </div>
   );
