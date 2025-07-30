31ba08fbb34b2154943e5536c01c05870bf520c0
Yashrajsinh Jadeja <yashrajsinh.jadeja@esparkbizmail.com>
2025-07-30 19:36:52 +0530

    Bug fixed at staff api.

--- src/api/StaffApi.ts
+++ src/api/StaffApi.ts
@@ -1,29 +1,22 @@
-import type { AssignRoleFormData, Role, Staff } from "../features/staff/models";
+import type { AssignRoleFormData, Role, Staff, StaffFormData } from "../features/staff/models";
 import apiHandler from "./handler";
 
-interface CreateStaffPayload {
-  firstName: string;
-  lastName: string;
-  mobileNumber: string;
-  email: string;
-  Address: string;
-  city: string;
-  role?: string;
-}
+
 
 export const getAllStaff = async () => {
   const response = await apiHandler.get<Staff[]>("/staff/all");
   return response.data;
 };
 
-export const createStaff = async (payload: CreateStaffPayload): Promise<Staff> => {
-  const role = payload.role;
-  delete payload.role;
-
-  const { data } = await apiHandler.post<Staff>("/staff", payload);
-
+export const createStaff = async (payload: StaffFormData): Promise<Staff> => {
+  const { role, ...newPayload } = payload;
+  const { data } = await apiHandler.post<Staff>("/staff", newPayload);
   if (data.id) {
-    await apiHandler.get<Role>(`/staff/assign-role-to-staff/${data.id}/${role}`);
+    const roleAssignment : AssignRoleFormData={
+      staffId : data.id,
+      role: role
+    }
+    await assignRole(roleAssignment)
   } else {
     throw new Error("Sent a staff add request to backend & got no staff ID.");
   }
